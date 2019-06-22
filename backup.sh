ef370b559e338893174727241feddcaf76516bf9
Linus Brännström <linusbrannstrom@MacBook.local>
2019-06-22 03:19:23 +0200

    basic config setup

--- backup.sh
+++ backup.sh
@@ -1,6 +1,103 @@
 #!/bin/bash
 
+############Author############ ############ Website ############
+###### Linus Brännström ###### ###### linusbrannstrom.dev ######
+############################## #################################
+
+
+
+function init {
+
+	echo "";
+	echo "****************************** Back Me Up ******************************";
+	echo "";
+	echo "************ Enter the information, leave blank for default ************";
+	echo "";
+
+	case "$OSTYPE" in
+  		solaris*) os=SOLARIS ;;
+  		darwin*)  os=MacOS ;; 
+  		linux*)   os=LINUX ;;
+  		bsd*)     os=BSD ;;
+  		msys*)    os=WINDOWS ;;
+		*)        os=unknown: $OSTYPE ;;
+	esac
+
+	echo "Operating system: $os";
+	echo "";
+
+	case "$os" in
+		MacOS)   user_path=/Users/ ;;
+		LINUX)   user_path=/home/ ;;
+		WINDOWS) user_path=/C:/Users ;;
+		*)		 user_path=/home/ ;;
+	esac
+
+	echo "Path to user folder: $user_path";
+	echo "";
+
+	printf "Backup location: ";
+	read res;
+	backup=$res;
+	if [ "$res" == "" ]; then
+		sudo mkdir /etc/back_me_up/;
+		backup=/etc/back_me_up/;
+	else
+		backup=$res;
+	fi
+	while [ ! -d $backup ]; do
+		printf "$backup is not a location, try again: ";
+		read res;
+		if [ "$res" == "" ]; then
+		    sudo mkdir /etc/back_me_up/;
+			backup=/etc/back_me_up/;
+		else
+			backup=$res;
+		fi
+	done
+
+	printf "Folder to be backed up: ";
+	read res;
+	folder=$res;
+
+	if [ "$res" == "" ]; then
+		folder="$user_path"$(whoami);
+	else
+		folder=$res;
+	fi
+	while [ ! -d $folder ]; do
+		printf "$folder is not a location, try again: ";
+		read res;
+		if [ "$res" == "" ]; then
+		folder="$user_path"$(whoami);
+		else
+			folder=$res;
+		fi
+	done
+
+	echo "The backup location is: $backup";
+	echo "The folder to backup is: $folder";
+
+	sudo cp ./backup.sh /usr/local/bin/backup.sh;
+	sudo touch ./backup.conf;
+	sudo bash -c "echo user=$(whoami) >> ./backup.conf";
+	sudo bash -c "echo location=$backup >> ./backup.conf";
+	sudo bash -c "echo folder=$folder >> ./backup.conf";
+	sudo mv ./backup.conf /etc/defaults/backup.conf;
+
+	exit;
+}
+
+if [ ! -f /etc/back_me_up.conf ]; then
+	init;
+fi
+
 # This bash script is used to perform various backups
+
+. /etc/defaults/back_me_up.conf;
+user="$user";
+default_backup_location="$location";
+default_backup_folder="$folder";
 
 while getopts 'u:f:b:' option; do
     case "${option}" in
@@ -8,7 +105,7 @@ while getopts 'u:f:b:' option; do
 	    user="${OPTARG}";;
 	f)
 	    file="${OPTARG}";;
-	b)
+	l)
 	    backup_location="${OPTARG}";;
 	\?)
 	    exit 42;;
@@ -18,18 +115,8 @@ done
 # shift opts away
 shift $((OPTIND - 1))
 
-if [ -z $user ]; then
-	user=$(whoami)
-fi
-
 if [ -z $backup_location ]; then
-	if grep -qs '/mnt/backups ' /proc/mounts; then
-        	echo "Remote backup location is mounted."
-	else
-        	echo "Remote backup location is not mounted, mounting...."
-        	sudo mount -t cifs //192.168.0.144/Backups/Raspberrypi /mnt/backups --verbose -o user=Linus
-	fi
-	path="/mnt/backups"
+	path="$default_backup_location"
 else
 	path=$backup_location
 fi
@@ -37,7 +124,7 @@ fi
 if [ -z $file ]; then
     if [ -z $1 ]; then
         input=/home/$user
-        output=${path}/${user}_"Home_"$(date +%Y-%m-%d_%H%M%S).tar.gz
+        output=${path}/${user}_"Home"_$(date +%Y-%m-%d_%H%M%S).tar.gz
     else
         if [ ! -d "/home/$user/$1" ]; then
                 echo "Requested $1 directory doesn't exist."
@@ -78,8 +165,6 @@ function backup_file {
     sudo cp $input $output
 }
 
-delold=$1
-
 function backup_directory {
 	src_files=$( total_files $input )
 	src_directories=$( total_directories $input )
@@ -97,17 +182,6 @@ function backup_directory {
 
 	if [ $src_files -eq $arch_files ]; then
         	echo "Backup of $input completed."
-        	if [ -z $backup_location ]; then
-                	echo "Deleting old backups..."
-			echo "Test: $delold"
-                	if [ -z $delold ]; then
-				echo "no args"
-                        	/home/pi/Scripts/delold.sh
-                	else
-				echo "Sending args: $delold"
-                        	/home/pi/Scripts/delold.sh $delold
-                	fi
-        	fi
         	echo "Details about the output backup file:"
         	ls -l $output
 	else
@@ -119,4 +193,3 @@ if [ -z $file ]; then
     backup_directory
 else
     backup_file
-fi
