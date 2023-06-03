a10fc5f966c6d61fdf39aad6f3ecd20217db1961
linulas <linubran90@gmail.com>
2023-06-03 16:13:54 +0200

    ads connect::to_home_server fn

--- src-tauri/src/ssh/connect.rs
+++ src-tauri/src/ssh/connect.rs
@@ -1,17 +1,30 @@
-use openssh::{KnownHosts, Session};
+use openssh::{KnownHosts::Strict, Session};
 
-pub async fn to_home_server() -> Result<(), openssh::Error> {
-    let session = Session::connect("ssh://linus@192.168.50.178:777", KnownHosts::Strict).await?;
+#[derive(Debug)]
+pub enum Error {
+    ConnectionFailed(openssh::Error),
+}
+
+impl From<openssh::Error> for Error {
+    fn from(e: openssh::Error) -> Self {
+        Self::ConnectionFailed(e)
+    }
+}
+
+pub async fn to_home_server() -> Result<Session, Error> {
+    let user = std::env::var("SSH_USER").expect("SSH_USER must be set");
+    let host = std::env::var("SSH_HOST").expect("SSH_HOST must be set");
+    let port = std::env::var("SSH_PORT").expect("SSH_PORT must be set");
+    let session = Session::connect(&format!("ssh://{user}@{host}:{port}"), Strict).await?;
 
     let ls = session.command("ls").output().await?;
     eprintln!(
-        "{}",
+        "Connected as user {user}\n\n{}",
         String::from_utf8(ls.stdout).expect("server output was not valid UTF-8")
     );
 
     let whoami = session.command("whoami").output().await?;
-    assert_eq!(whoami.stdout, b"me\n");
+    assert_eq!(whoami.stdout, format!("{user}\n").into_bytes());
 
-    session.close().await?;
-    Ok(())
+    Ok(session)
 }
