721187437ee83583cd44f4de634627e125b7df09
linulas <linubran90@gmail.com>
2023-06-03 16:14:29 +0200

    load dotenv at startup

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use dotenv::dotenv;
+
 mod models;
 mod ssh;
 
@@ -8,6 +10,7 @@ mod ssh;
 mod tests;
 
 fn main() {
+    dotenv().ok();
     tauri::Builder::default()
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
