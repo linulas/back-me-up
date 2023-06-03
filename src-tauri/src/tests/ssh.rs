cc8f5a7515bf1a48ee0b0bbf79822b3ee54b530e
linulas <linubran90@gmail.com>
2023-06-03 16:14:52 +0200

    ads test_connection

--- src-tauri/src/tests/ssh.rs
+++ src-tauri/src/tests/ssh.rs
@@ -1,5 +1,12 @@
-#[test]
-fn test_connection() {
-    assert_eq!(2 + 2, 4);
-}
+use crate::ssh::connect;
+use dotenv::dotenv;
 
+#[actix_rt::test]
+async fn test_connection() {
+    dotenv().ok();
+    let connection = connect::to_home_server().await;
+    if let Err(e) = &connection {
+        eprintln!("{e:?}");
+    }
+    assert!(connection.is_ok());
+}
