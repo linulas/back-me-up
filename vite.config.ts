1d6614efdd50f3c16c81c1f10b35ecb6468e3e63
linulas <linubran90@gmail.com>
2023-06-08 09:44:49 +0200

    ads unplugin-icons

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,20 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import Icons from 'unplugin-icons/vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [
+		sveltekit(),
+		Icons({
+			compiler: 'svelte'
+		})
+	],
+
+	css: {
+		preprocessorOptions: {
+			scss: {
+				additionalData: `@import "$lib/style/theme.scss";`
+			}
+		}
+	}
 });
