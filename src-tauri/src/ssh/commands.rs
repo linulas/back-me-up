5fb589ae868857a4a5bfcdaf3319bc69c7f6e02d
linulas <linubran90@gmail.com>
2023-07-13 08:49:39 +0200

    ads rsync instead of scp to ignore hidden files

--- src-tauri/src/ssh/commands.rs
+++ src-tauri/src/ssh/commands.rs
@@ -1,11 +1,9 @@
-use std::path::Path;
-use std::process::Command;
-
-use openssh_sftp_client::Sftp;
-
 use super::Error;
 use crate::models::app::Config;
 use crate::models::backup::Backup;
+use openssh_sftp_client::Sftp;
+use std::path::Path;
+use std::process::Command;
 
 pub async fn assert_client_directory_on_server(client: &Sftp, path: &Path) -> Result<(), Error> {
     match client.open(&path).await {
@@ -14,33 +12,32 @@ pub async fn assert_client_directory_on_server(client: &Sftp, path: &Path) -> Re
             println!("Error: {e:?}");
             client.fs().create_dir(&path).await?;
             Ok(())
-        },
+        }
     }
 }
 
 pub fn backup_to_server(backup: &Backup, config: &Config) -> Result<(), Error> {
     let connection_string = format!(
-        "{}@{}:{}",
+        "{}@{}:'{}'",
         config.username,
         config.server_address.replace("http://", ""),
         backup.server_folder.path
     );
 
-    let scp = Command::new("scp")
-        .args([
-            "-r",
-            "-P",
-            &config.server_port.to_string(),
-            &backup.client_folder.path,
-            &connection_string,
-        ])
+    let rsync = Command::new("rsync")
+        .arg("-av")
+        .arg("-e")
+        .arg(format!("ssh -p {}", config.server_port))
+        .arg("--exclude=.*")
+        .arg(&backup.client_folder.path)
+        .arg(&connection_string)
         .status()
-        .expect("Failed to execute scp");
+        .expect("Failed to execute rsync command");
 
-    if scp.success() {
+    if rsync.success() {
         Ok(())
     } else {
-        Err(Error::Command(String::from("SCP command failed")))
+        Err(Error::Command(String::from("rysnc command failed")))
     }
 }
 
