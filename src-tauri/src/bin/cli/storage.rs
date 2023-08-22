ba828a1748a045811e1c32cc7f9347ed63623dd1
linulas <linubran90@gmail.com>
2023-08-23 01:05:46 +0200

    ads storage module

---
+++ src-tauri/src/bin/cli/storage.rs
@@ -0,0 +1,117 @@
+use std::path::PathBuf;
+use std::{env, fs};
+
+use bmu::models::app::Config;
+use bmu::models::backup::Backup;
+
+#[derive(Debug)]
+pub enum Error {
+    NotUnique(String),
+    Env(env::VarError),
+    Io(std::io::Error),
+}
+
+impl From<env::VarError> for Error {
+    fn from(err: env::VarError) -> Self {
+        Self::Env(err)
+    }
+}
+
+impl From<std::io::Error> for Error {
+    fn from(err: std::io::Error) -> Self {
+        Self::Io(err)
+    }
+}
+
+pub struct Storage {
+    pub cache_dir: PathBuf,
+    pub config_dir: PathBuf,
+    pub data_dir: PathBuf,
+}
+
+impl Storage {
+    pub fn load() -> Result<Self, Error> {
+        let cache_dir = PathBuf::from(env::var("APP_CACHE_DIR")?);
+        let config_dir = PathBuf::from(env::var("APP_CONFIG_DIR")?);
+        let data_dir = PathBuf::from(env::var("APP_DATA_DIR")?);
+
+        if !cache_dir.exists() {
+            fs::create_dir_all(&cache_dir)?;
+        }
+        if !cache_dir.is_dir() {
+            fs::create_dir_all(&cache_dir)?;
+        }
+        if !config_dir.exists() {
+            fs::create_dir_all(&config_dir)?;
+        }
+        if !config_dir.is_dir() {
+            panic!("APP_CONFIG_DIR is not a directory");
+        }
+        if !data_dir.exists() {
+            panic!("APP_DATA_DIR does not exist");
+        }
+        if !data_dir.is_dir() {
+            panic!("APP_DATA_DIR is not a directory");
+        }
+
+        Ok(Self {
+            cache_dir,
+            config_dir,
+            data_dir,
+        })
+    }
+
+    pub fn config(&self) -> Option<Config> {
+        if !self.config_dir.join("server.conf.json").exists() {
+            return None;
+        }
+
+        let config_file_path = self.config_dir.join("server.conf.json");
+        let config_file_contents =
+            std::fs::read_to_string(&config_file_path).expect("Failed to read config file");
+
+        Some(serde_json::from_str(&config_file_contents).expect("Failed to parse config file"))
+    }
+
+    // fn write_conig(&self, config: Config) {
+    //     let config_file_path = self.config_dir.join("server.conf.json");
+    //     let config_file_contents =
+    //         serde_json::to_string(&config).expect("Failed to serialize config");
+    //     std::fs::write(&config_file_path, config_file_contents)
+    //         .expect("Failed to write config file");
+    // }
+
+    pub fn backups(&self) -> Vec<Backup> {
+        if !self.data_dir.join("backups.json").exists() {
+            return vec![];
+        }
+
+        let backup_file_path = self.data_dir.join("backups.json");
+        let backup_file_contents =
+            std::fs::read_to_string(&backup_file_path).expect("Failed to read backup file");
+
+        serde_json::from_str(&backup_file_contents).expect("Failed to parse backup file")
+    }
+
+    pub fn add_backup(&self, backup: Backup) -> Result<(), Error> {
+        let mut backups = self.backups();
+        if backups.iter().any(|b| {
+            b.client_location.path == backup.client_location.path
+                && b.server_location.path == backup.server_location.path
+        }) {
+            return Err(Error::NotUnique("Backup already exists".to_string()));
+        };
+        backups.push(backup);
+        self.write_backups(backups);
+
+        Ok(())
+    }
+
+    fn write_backups(&self, backups: Vec<Backup>) {
+        let backup_file_path = self.data_dir.join("backups.json");
+        let backup_file_contents =
+            serde_json::to_string(&backups).expect("Failed to serialize backup file");
+        std::fs::write(&backup_file_path, backup_file_contents)
+            .expect("Failed to write backup file");
+    }
+}
