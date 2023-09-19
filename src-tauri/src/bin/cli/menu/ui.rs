88a340e10dbdf128e93695112be47504b284ee17
linulas <linubran90@gmail.com>
2023-09-19 14:41:55 +0200

    adjusts title in print_frame if emojis is included

--- src-tauri/src/bin/cli/menu/ui.rs
+++ src-tauri/src/bin/cli/menu/ui.rs
@@ -2,9 +2,14 @@ use std::io::{self, Write};
 
 use futures::Future;
 
-pub fn print_frame(title: &str, messages_to_print: Vec<String>) {
+pub fn print_frame(title: &str, messages_to_print: Vec<String>, emojis_in_title: bool) {
+    let gap_length = if emojis_in_title {
+        title.len()
+    } else {
+        title.len() + 2
+    };
     let one_side_of_dashes = (0..12).map(|_| "-").collect::<String>();
-    let fill_gap_from_title = (0..title.len()).map(|_| "-").collect::<String>();
+    let fill_gap_from_title = (0..gap_length).map(|_| "-").collect::<String>();
 
     println!("\n{one_side_of_dashes} {title} {one_side_of_dashes}\n");
 
