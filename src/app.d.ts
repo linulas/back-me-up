22cf80087a6c271632a6e96ecac35efd6423e200
linulas <linubran90@gmail.com>
2024-01-05 10:45:15 +0100

    ads Job types

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,24 +1,28 @@
+import type { Folder } from '../src-tauri/bindings/Folder';
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 declare global {
-
 	type ButtonType = 'primary' | 'secondary' | 'danger' | 'icon' | 'icon-with_background';
 	type ButtonState = 'idle' | 'loading' | 'success' | 'error';
 	type BackupFolderJobType = 'single' | 'reacurring';
-	type BackupFolderJob = {
-		__type: BackupFolderJobType;
-		folder: Folder;
-	};
 
 	namespace App {
-    type Theme = 'light' | 'dark';
-    interface Config {
-      theme: Theme;
-    }
-		// interface Error {}
-		// interface Locals {}
-		// interface PageData {}
-		// interface Platform {}
+		type Theme = 'light' | 'dark';
+		interface Config {
+			theme: Theme;
+		}
+
+		interface Job {
+			__type: BackupFolderJobType;
+      id: string;
+			state: ButtonState;
+      task?: Promise<void>;
+		}
+
+		interface BackupFolderJob extends Job {
+			from?: Folder;
+      to?: Folder;
+		}
 	}
 	// HACK: types not working for unplugin-icons
 	declare module '~icons/*' {
